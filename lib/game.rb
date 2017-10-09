83d12d6617ff8e711faad26ebabf0958a3ea48c5
Emily Morgan <emorgan05@gmail.com>
2017-10-09 20:03:32 +0000

    player.rb and human.rb

---
+++ lib/game.rb
@@ -0,0 +1,31 @@
+require "pry"
+
+class Game
+  attr_accessor :board, :player_1, :player_2
+
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+
+  def initialize(player_1 = Players::Human, player_2 = Players::Human, board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+
+  def current_player
+    if board.turn_count % 2 == 0
+      "O"
+    else
+      "X"
+    end
+  end
+
+end
